e319ac7e6c73a89eae4e1b51c8aea0d626568e72
Parker McCoog <mccoogp@icloud.com>
2024-05-06 10:37:17 -0700

    3 activities left. Changing a lot.

--- Assets/Scenes/CharcMove.cs
+++ Assets/Scenes/CharcMove.cs
@@ -11,58 +11,64 @@ public class CharcMove : MonoBehaviour
     float velocity = 0;
     public Vector2 turn;
     public float sensitivity = 3f;
+    public bool pause = false;
     void Update()
     {
-        turn.x += Input.GetAxis("Mouse X") * sensitivity;
-        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
-        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
-        if (Input.GetKey("d"))
+        if (pause == false)
         {
-            Vector3 direction = new Vector3(Mathf.Sin((turn.x + 90) / 180 * Mathf.PI), 0, Mathf.Cos((turn.x + 90) / 180 * Mathf.PI));
-            control.Move(direction * Time.deltaTime * 10);
 
-        }
-        if (Input.GetKey("w"))
-        {
-            Vector3 direction = new Vector3(Mathf.Sin(turn.x / 180 * Mathf.PI), 0, Mathf.Cos(turn.x / 180 * Mathf.PI));
-            control.Move(direction * Time.deltaTime * 10);
-        }
-        if (Input.GetKey("s"))
-        {
-            Vector3 direction = new Vector3(-Mathf.Sin(turn.x / 180 * Mathf.PI), 0, -Mathf.Cos(turn.x / 180 * Mathf.PI));
-            control.Move(direction * Time.deltaTime * 10);
 
-        }
-        if (Input.GetKey("a"))
-        {
-            Vector3 direction = new Vector3(Mathf.Sin((turn.x - 90) / 180 * Mathf.PI), 0, Mathf.Cos((turn.x - 90) / 180 * Mathf.PI));
-            control.Move(direction * Time.deltaTime * 10);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (jumped == 0)
+            turn.x += Input.GetAxis("Mouse X") * sensitivity;
+            turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+            transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+            if (Input.GetKey("d"))
             {
-                jumped += 1;
-                velocity = 20;
+                Vector3 direction = new Vector3(Mathf.Sin((turn.x + 90) / 180 * Mathf.PI), 0, Mathf.Cos((turn.x + 90) / 180 * Mathf.PI));
+                control.Move(direction * Time.deltaTime * 10);
 
             }
-            else if (jumped == 1)
+            if (Input.GetKey("w"))
             {
-                jumped += 1;
+                Vector3 direction = new Vector3(Mathf.Sin(turn.x / 180 * Mathf.PI), 0, Mathf.Cos(turn.x / 180 * Mathf.PI));
+                control.Move(direction * Time.deltaTime * 10);
+            }
+            if (Input.GetKey("s"))
+            {
+                Vector3 direction = new Vector3(-Mathf.Sin(turn.x / 180 * Mathf.PI), 0, -Mathf.Cos(turn.x / 180 * Mathf.PI));
+                control.Move(direction * Time.deltaTime * 10);
 
             }
-        }
-        RaycastHit m_Hit;
-        velocity -= 2 * gravity * Time.deltaTime;
-        control.Move(new Vector3(0f, velocity * Time.deltaTime, 0f));
-        Vector3 below = new Vector3(0, -1, 0);
-        if (Physics.BoxCast(transform.position, box / 2, below, out m_Hit, transform.rotation, velocity * -1 * Time.deltaTime, layertohit))
-        {
-            if (m_Hit.distance < -velocity * Time.deltaTime / 2)
+            if (Input.GetKey("a"))
             {
+                Vector3 direction = new Vector3(Mathf.Sin((turn.x - 90) / 180 * Mathf.PI), 0, Mathf.Cos((turn.x - 90) / 180 * Mathf.PI));
+                control.Move(direction * Time.deltaTime * 10);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (jumped == 0)
+                {
+                    jumped += 1;
+                    velocity = 20;
 
-                jumped = 0;
-                velocity = velocity / 2;
+                }
+                else if (jumped == 1)
+                {
+                    jumped += 1;
+
+                }
+            }
+            RaycastHit m_Hit;
+            velocity -= 2 * gravity * Time.deltaTime;
+            control.Move(new Vector3(0f, velocity * Time.deltaTime, 0f));
+            Vector3 below = new Vector3(0, -1, 0);
+            if (Physics.BoxCast(transform.position, box / 2, below, out m_Hit, transform.rotation, velocity * -1 * Time.deltaTime, layertohit))
+            {
+                if (m_Hit.distance < -velocity * Time.deltaTime / 2)
+                {
+
+                    jumped = 0;
+                    velocity = velocity / 2;
+                }
             }
         }
     }
